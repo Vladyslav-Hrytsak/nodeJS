e573adc73ac924c36f49d2787c176a20a76c8b44
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2026-02-04 18:12:39 +0200

    homework 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
+
+const hw_2 = async () => {
+    await fs.mkdir(path.join(__dirname, 'baseFolder'), { recursive: true });
+
+    for (let i = 0; i < 5; i++) {
+        const folderPath = path.join(__dirname, 'baseFolder', `folder-${i}`);
+        await fs.mkdir(folderPath, { recursive: true });
+
+        const stat = await fs.stat(folderPath);
+        console.log(folderPath,' is directory? -' ,stat.isDirectory() ,' is file? -' , stat.isFile() );
+
+        for (let j = 0; j < 5; j++) {
+            const filePath = path.join(folderPath, `file-${j}.txt`);
+            await fs.writeFile(filePath, 'Hello World!');
+
+            const statF = await fs.stat(filePath);
+            console.log(filePath,' is directory? -' ,statF.isDirectory() ,' is file? -' , statF.isFile() );
+        }
+    }
+}
+
+
+hw_2()
